8dbb02a8d5b19879d318e98d91779ffd0b6ac500
NiteshRajput26 <niteshrajput528@gmail.com>
2024-03-23 00:25:59 +0530

    react series

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Code from "./Code"
+
+function App() {
+
+
+  return (
+    //      <> </> or <div></div> dono rkh skte h
+    <>
+  <Code/>
+  <h1>react code</h1>
+  <p>test para</p>
+  </>
+  )
+}
+
+export default App
+
+//  
---
+++ 01vitereact/src/Code.jsx
@@ -0,0 +1,9 @@
+function Code() {
+
+
+    return (
+     <h3>Code is ready </h3>
+    )
+  }
+  
+  export default Code
